f009aeb602fa5658bd9329b4b00ec3401dd32316
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-01-29 20:35:26 -0800

    all

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+function App() {
+  return (
+    <>
+    <h1 className="">hello</h1>
+    </>
+  )
+}
+
+export default App
