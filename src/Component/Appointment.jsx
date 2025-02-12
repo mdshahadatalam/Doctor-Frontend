33b895e1d1f99b23f03db750414c8ed3011f05b4
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-02-12 11:57:11 -0800

    more

--- src/Component/Appointment.jsx
+++ src/Component/Appointment.jsx
@@ -12,19 +12,23 @@ export const Appointment = () => {
       const [gender,setGender] = useState("")
       const [date,setDate] = useState("")
       const [docName,setDocName] = useState("")
+      const [img,setImg] = useState({})
 
       const handleSubmit =(e)=>{
           e.preventDefault()
           setLoader(true)
-        axios.post('http://localhost:3000/appointment',{
-            name:name,
-            email:email,
-            phone:phone,
-            address:address,
-            gender:gender,
-            date:date,
-            docName:docName
-        }).then(res=>{
+
+          let data = new FormData()
+          data.append("img",img)
+          data.append("name",name)
+          data.append("email",email)
+          data.append("phone",phone)
+          data.append("address",address)
+          data.append("gender",gender)
+          data.append("date",date)
+          data.append("docName",docName)
+
+        axios.post('http://localhost:3000/appointment',data).then(res=>{
             console.log(res)
             setName('')
             setEmail('')
@@ -69,6 +73,20 @@ export const Appointment = () => {
       <div className="w-full max-w-md p-6 bg-white shadow-lg rounded-2xl">
         <h2 className="text-xl font-serif font-semibold text-center text-gray-700 mb-4">Appointment</h2>
         <form className="space-y-4">
+          
+        <div className="mb-2">
+    <label className="block text-sm font-semibold text-gray-700 mb-2" htmlFor="file-upload">
+      Upload your image
+    </label>
+    <input
+      onChange={(e)=>{setImg(e.target.files[0])}}
+      className="py-2 px-4 w-full border border-[#E0E0E0] rounded-lg shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+      type="file"
+      id="file-upload"
+    />
+  </div>
+
+
           <input
            className="w-full mt-3 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400" type="text"
             name="name"
